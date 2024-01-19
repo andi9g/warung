d341f575edbd4ec46c52b4b89cf8d0c571da201a
andi9g <andirizky.bayuputra@gmail.com>
2024-01-20 02:21:39 +0700

    oke

--- resources/views/pages/profil.blade.php
+++ resources/views/pages/profil.blade.php
@@ -10,7 +10,7 @@
         <div class="col-md-2"></div>
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header"><h3>Barang Masuk</h3></div>
+                <div class="card-header"><h3>UBAH PASSWORD</h3></div>
                 <form action="{{ route('ubah.password', []) }}" method="post" >
                     @csrf
                     <div class="card-body">
